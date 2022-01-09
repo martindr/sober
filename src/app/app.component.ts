16f45eb6abf406dea80d77422b90ba2574b8b40b
Martin Dreyer <Martin.Dreyer@teck.com>
2022-01-08 17:50:44 -0800

    arbitrary change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+export interface Person {
+  name: string;
+  startDate: Date;
+  days: Number
+}
+
+const ELEMENT_DATA: Person[] = [
+  {name: 'Person 1', startDate: new Date("3/12/2020"), days: 1},
+  {name: 'Person 2', startDate: new Date("11/13/2021"), days: 123},
+  {name: 'Person 3', startDate: new Date("5/14/2019"), days: 18}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +19,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sober';
+  displayedColumns: string[] = ['name'];
+  dataSource = ELEMENT_DATA;
+
+  calculateDays(dt: Date): Number {
+    return Math.round((Date.now() - dt.getTime()) / (1000 * 3600 * 24));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatChipsModule } from '@angular/material/chips'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +21,17 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    FormsModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
